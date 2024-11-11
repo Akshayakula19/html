ab2a8d8676f6b04774a07d47bd0e9e6efffb4ca2
Anthony Kishan <anthony.kishan01@gmail.com>
2024-11-12 01:55:51 +0600

    Responsive Design Updated

--- Assets/js/testimonial.js
+++ Assets/js/testimonial.js
@@ -1,70 +1,70 @@
 const testimonials = [
     {
-        name: "John Doe",
-        image: "Assets/Images/Profile Img.jpg",
-        text: "Exceptional service! They exceeded my expectations in every way.",
-        date: "[ AUGUST, 2021 ]",
+        name: "intuitionstudio",
+        image: "Assets/Images/Testimonial/intuitionstudio.webp",
+        text: "Great work! He did exactly what I wanted and did even more, I will definitely need his service again.",
+        date: "[ SEPTEMBER, 2021 ]",
         company: "https://www.fiverr.com/favicon.ico",
-        country: "USA"
+        country: "United Kingdom"
     },
     {
-        name: "John Doe",
-        image: "Assets/Images/Profile Img.jpg",
-        text: "Exceptional service! They exceeded my expectations in every way.",
-        date: "[ AUGUST, 2021 ]",
+        name: "abigaildarko",
+        image: "Assets/Images/Testimonial/abigaildarko.webp",
+        text: "Kishan is an exceptional artist!!!.. He designed what I requested to my satisfaction and absolute perfection!!!.. He deserves more than 5stars! I 100% recommend his services if needed. Will definitely order his gig again.",
+        date: "[ MARCH, 2022 ]",
         company: "https://www.fiverr.com/favicon.ico",
-        country: "USA"
+        country: "Ghana"
     },
     {
-        name: "John Doe",
-        image: "Assets/Images/Profile Img.jpg",
-        text: "Exceptional service! They exceeded my expectations in every way.",
-        date: "[ AUGUST, 2021 ]",
+        name: "grandkadolz",
+        image: "Assets/Images/Testimonial/grandkadolz.png",
+        text: "Quick, friendly and efficient. I will most likely use his services again.",
+        date: "[ APRIL, 2023 ]",
         company: "https://www.fiverr.com/favicon.ico",
-        country: "USA"
+        country: "Austria"
     },
     {
-        name: "John Doe",
-        image: "Assets/Images/Profile Img.jpg",
-        text: "Exceptional service! They exceeded my expectations in every way.",
-        date: "[ AUGUST, 2021 ]",
+        name: "abigaildarko",
+        image: "Assets/Images/Testimonial/abigaildarko.webp",
+        text: "Second time ordering this gig and i must say Anthony always exceeds my expectations and I really love his work… Always very neat and perfect.. exactly what I wanted!!! He's actually the best and I 100% recommend his services if needed..Thanks a lot Anthony for always giving out your best.",
+        date: "[ MAY, 2022 ]",
         company: "https://www.fiverr.com/favicon.ico",
-        country: "USA"
+        country: "Ghana"
     },
 
 
 
     {
-        name: "John Doe",
-        image: "Assets/Images/Profile Img.jpg",
-        text: "Exceptional service! They exceeded my expectations in every way.",
-        date: "[ AUGUST, 2021 ]",
+        name: "grandkadolz",
+        image: "Assets/Images/Testimonial/grandkadolz.png",
+        text: "Kishan did exactly what I asked in a timely manner. I will most likely use his services again.",
+        date: "[ JUNE, 2023 ]",
         company: "https://www.fiverr.com/favicon.ico",
-        country: "USA"
+        country: "Austria"
     },
     {
-        name: "John Doe",
-        image: "Assets/Images/Profile Img.jpg",
-        text: "Exceptional service! They exceeded my expectations in every way.",
-        date: "[ AUGUST, 2021 ]",
+        name: "aleezadesign256",
+        image: "Assets/Images/Testimonial/aleezadesign256.png",
+        text: "Excellent friendly and fast professional service, would definitely recommend!",
+        date: "[ AUGUST, 2023 ]",
         company: "https://www.fiverr.com/favicon.ico",
-        country: "USA"
+        country: "United Kingdom"
     },
     {
-        name: "John Doe",
-        image: "Assets/Images/Profile Img.jpg",
-        text: "Exceptional service! They exceeded my expectations in every way.",
-        date: "[ AUGUST, 2021 ]",
+        name: "gideonmk",
+        image: "Assets/Images/Testimonial/gideonmk.webp",
+        text: "Great work, communicated well and made all the revisions I asked for. Will come back for future work.",
+        date: "[ JULY, 2023 ]",
         company: "https://www.fiverr.com/favicon.ico",
-        country: "USA"
+        country: "United States"
     },
     {
-        name: "John Doe",
-        image: "Assets/Images/Profile Img.jpg",
-        text: "Exceptional service! They exceeded my expectations in every way.",
-        date: "[ AUGUST, 2021 ]",
+        name: "walter34r",
+        image: "Assets/Images/Testimonial/walter34r.webp",
+        text: "Good skills and nice character",
+        date: "[ AUGUST, 2023 ]",
         company: "https://www.fiverr.com/favicon.ico",
-        country: "USA"
+        country: "United States"
     },
 
 ];
