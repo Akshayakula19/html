b4bd099d754b83ff33828816c279bca4b7caab95
Anthony Kishan <anthony.kishan01@gmail.com>
2024-11-15 02:59:31 +0600

    Design Updated

--- Assets/js/service-section.js
+++ Assets/js/service-section.js
@@ -12,7 +12,7 @@ const services = [
             'Content Management',
             'Ongoing Support',
         ],
-        imageUrl: 'https://via.placeholder.com/400x300',
+        imageUrl: 'Assets/Images/Works/01-DEVPULSE.jpg',
     },
     {
         id: 'web-design',
@@ -28,7 +28,7 @@ const services = [
             'Cross-Browser Compatibility',
             'Branding Integration',
         ],
-        imageUrl: 'https://via.placeholder.com/400x300',
+        imageUrl: 'Assets/Images/Works/02-EDISON.jpg',
     },
     {
         id: 'graphics-design',
@@ -43,7 +43,7 @@ const services = [
             'Illustrations & Icons',
             'Print Design',
         ],
-        imageUrl: 'https://via.placeholder.com/400x300',
+        imageUrl: 'Assets/Images/Works/The-last-rope-01.jpg',
     },
 ];
 
